75976058bcdae8ddad7f3ccb2b0efec92d0acb64
Caleb Lim <cl@vnwlim.com>
2024-09-30 20:36:31 +0100

    Added Score UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
 
 public class PlayerController : MonoBehaviour {
 
@@ -9,8 +10,18 @@ public class PlayerController : MonoBehaviour {
     public float speed;
     private int countPickUps;
 
+    private UIDocument _document;
+
+    private Label _score;
+    private Label _win;
+    private int numPickUps = 7;
+
     void Start() {
         countPickUps = 0;
+        _document = GetComponent<UIDocument>();
+        _score = _document.rootVisualElement.Q<Label>("Score");
+        _score.text = "Score: " + countPickUps.ToString();
+        _win = _document.rootVisualElement.Q<Label>("Win");
     }
 
 
@@ -24,10 +35,20 @@ public class PlayerController : MonoBehaviour {
         GetComponent<Rigidbody>().AddForce (movement * speed * Time.fixedDeltaTime);
     }
 
-    void OnTriggerEnter ( Collider other ) {
+    void OnTriggerEnter (Collider other) {
         if( other.gameObject.tag == "PickUp") {
             other.gameObject.SetActive(false);
             countPickUps++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        _score.text = "Score: " + countPickUps.ToString();
+        if (countPickUps >= numPickUps)
+        {
+            _win.text = "You Win!";
         }
     }
 }
